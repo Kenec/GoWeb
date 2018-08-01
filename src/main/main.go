76123bfed70008503426328e274f2c227d4f7feb
Kenec <you@example.com>
2018-08-01 13:32:23 +0100

    load template from html page

--- src/main/main.go
+++ src/main/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"html/template"
-	"fmt"
-	"os"
+	"net/http"
+	"log"
 )
 
 func main() {
-	templateString := `This is a sample template string`
-	t, err := template.New("title").Parse(templateString)
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = t.Execute(os.Stdout, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	templates := populateTemplate()
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		requestedFile := request.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(writer, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			writer.WriteHeader(http.StatusNotFound)
+		}
+		http.Handle("/image/", http.FileServer(http.Dir("public")))
+		http.Handle("/scss/", http.FileServer(http.Dir("public")))
+
+	})
+	http.ListenAndServe(":3000", nil)
+}
+
+
+func populateTemplate() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
